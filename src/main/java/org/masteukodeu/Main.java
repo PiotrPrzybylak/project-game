e867275e56686c2cfed353d8e523b479ba589ed3
Piotr Przybylak <piotr.przybylak@gmail.com>
2024-01-04 22:03:35 +0100

    Simulate ideal world

--- src/main/java/org/masteukodeu/Main.java
+++ src/main/java/org/masteukodeu/Main.java
@@ -1,7 +1,17 @@
 package org.masteukodeu;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many features does your project have?");
+        int projectSize = scanner.nextInt();
+        System.out.println("How many features does one developer create in a week?");
+        int developerVelocity = scanner.nextInt();
+        System.out.println("How many developers will work on this project full time?");
+        int developers = scanner.nextInt();
+        double time = ((double) projectSize) / (developers * developerVelocity);
+        System.out.println("Your project will take " + time + " weeks");
     }
 }
